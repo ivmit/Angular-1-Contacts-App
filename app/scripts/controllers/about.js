e2b0aea59a7fb73c3acaea733e2c06db8875f3de
nony1992 <ionutvalentin66@gmail.com>
2016-01-02 18:52:22 +0200

    Scaffolding the app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngContactsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngContactsApp
+ */
+angular.module('ngContactsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
